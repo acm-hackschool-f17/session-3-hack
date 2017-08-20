5a1c6c9025a4f351b9fca76299784ebcf5a72c9e
shannonphu <shannonphu@sbcglobal.net>
2017-08-20 00:05:37 -0700

    setup root endpoint saying Hello World

---
+++ server.js
@@ -0,0 +1,15 @@
+// Loads the library express which makes creating a server easy in a node application
+var express = require('express');
+
+// Instantiate the "app" to start creating server endpoints
+var app = express();
+
+// Server listens to port 3000
+app.listen(3000, function () {
+	console.log('Your app is listening on port 3000!');
+});
+
+// Root web app endpoint
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
