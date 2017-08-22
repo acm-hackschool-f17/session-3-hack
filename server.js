a0ce6efca28f32e94cd4b5cbce5482f843670658
shannonphu <shannonphu@sbcglobal.net>
2017-08-21 23:27:57 -0700

    added pre-made elib view and styles

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express');
 // Instantiate the "app" to start creating server endpoints
 var app = express();
 
+// expose all files in public/ to be accessible from the root of our website
+app.use(express.static('public'));
+
 // Set up where our application will look for client-side files (HTML, CSS, JS)
 app.set('view engine', 'hbs');
 
@@ -25,3 +28,33 @@ app.get('/', function (req, res) {
 app.get('/ucla', function (req, res) {
 	res.send('UCLA is gr8!');
 });
+
+
+//////////////////////////////////////////////////////
+// Helper Functions:                                //
+// Functions after this point have been defined     //
+// for you. You can see how they work, but put      //
+// all code above this point, and don't modify      //
+// or play around with it during the session. You   //
+// are free to do that later, though!               //
+//////////////////////////////////////////////////////
+
+function initBooks() {
+	var initialBooks = [
+		{ 
+			title: "The Three Musketeers", 
+			author: "Alexandre Dumas", 
+			copies: 7, 
+			isbn: "978-1-56619-909-4" 
+		},
+		{ title: "Ivanhoe", author: "Sir Walter Scott", copies: 2, isbn: "978-1-46110-482-3" },
+		{ title: "The Count of Monte Cristo", author: "Alexandre Dumas", copies: 3, isbn: "978-1-39912-897-1" },
+		{ title: "Last of the Mohicans", author: "James Fenimore Cooper", copies: 2, isbn: "978-1-87140-981-4" },
+		{ title: "Moby Dick", author: "Herman Melville", copies: 8, isbn: "978-1-09713-891-7" },
+		{ title: "A Tale of Two Cities", author: "Charles Dickens", copies: 14, isbn: "978-1-67819-414-4" },
+		{ title: "Robin Hood", author: "Howard Pyle", copies: 1, isbn: "978-1-18904-912-4" },
+		{ title: "Arabian Nights", author: "Antony Galland", copies: 6, isbn: "978-1-89231-991-4" }
+	];
+
+	return initialBooks;
+}
